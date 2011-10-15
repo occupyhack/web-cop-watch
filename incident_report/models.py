a1ff1a822218a1fad26c43e8bf67e4e859a04cee
beatpanda@gmail.com <beatpanda@bianca.local>
2011-10-15 14:55:52 -0700

    finished incident report model

---
+++ incident_report/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.localflavor.us.models import USStateField, PhoneNumberField
+
+class Person(models.Model):
+    name 		= models.CharField(max_length=256)
+    withhold	= models.BooleanField()
+    class Meta:
+        abstract = True
+
+class Cop(Person):
+    badge_num	= models.CharField(max_length=128, blank=True, null=True)
+    cert_num	= models.CharField(max_length=128, blank=True, null=True)
+    license_num	= models.CharField(max_length=10)
+    agency		= models.ForeignKey('Agency')
+
+class Witness(Person):
+    contact_phone	= PhoneNumberField()
+    contact_email	= models.EmailField()
+    contact_other	= models.TextField()
+    statement		= models.TextField()
+
+class Victim(Person):
+    arrested		= models.BooleanField()
+    complaint		= models.BooleanField()
+    lawyer			= models.BooleanField()
+    charge			= models.CharField(max_length=128)
+    injuries		= models.TextField()
+    contact_phone	= PhoneNumberField()
+    contact_email	= models.EmailField()
+    contact_other	= models.TextField()
+
+class Agency(models.Model):
+    """Agencies will be presented as a dropdown and shouldn't be editable from the user-facing side"""
+    state			= USStateField()
+    name			= models.CharField(max_length=256)
+    jurisdiction	= models.CharField(max_length=256)
+
+class Evidence(models.Model):
+    title		= models.CharField(max_length=128, blank=True, null=True)
+    photo		= models.ImageField(upload_to="evidence")
+    vid_link	= models.URLField()
+    incident	= models.ForeignKey('Incident')
+    private		= models.BooleanField()
+	
+
+class Incident(models.Model):
+    date		= models.DateField()
+    time		= models.TimeField()
+    filed		= models.DateTimeField(auto_now=True,auto_now_add=True)
+    loc_lat	    = models.FloatField()
+    loc_lon     = models.FloatField()
+    loc_text	= models.CharField(max_length=128, blank=True, null=True)
+    loc_state	= models.CharField(max_length=128, blank=True, null=True)
+    loc_city	= models.CharField(max_length=128, blank=True, null=True)
+    cop			= models.ManyToManyField('Cop')	
+    witness		= models.ManyToManyField('Witness')
+    victim		= models.ManyToManyField('Victim')
+    nature		= models.CharField(max_length=128, blank=True, null=True)
+    private		= models.BooleanField()
+# Create your models here.
--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db/copwatch.sql',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -27,7 +27,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -115,6 +115,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'south',
+	'incident_report',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
